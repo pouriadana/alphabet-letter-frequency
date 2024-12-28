cb4395ed35140c36316ceffb334505781c47bdc6
Pouria Dana <pouriadana73@gmail.com>
2024-12-28 13:22:04 +0330

    Update main.c to fill frequency array There's also a debug code to print the frequency numbers which will be removed.

--- main.c
+++ main.c
@@ -11,5 +11,16 @@ int main()
         freq[i] = 0;
     }
 
-    
+    while ((c = getchar()) != EOF) {
+        if ('a' <= c && c <= 'z') {
+            ++freq[c - 'a'];
+        }
+        if ('A' <= c && c <= 'Z') {
+            ++freq[c - 'A'];
+        }
+    }
+    // Debug 
+    for (int i = 0; i < ALEPH; ++i) {
+        printf("%d\n", freq[i]);
+    }
 }
